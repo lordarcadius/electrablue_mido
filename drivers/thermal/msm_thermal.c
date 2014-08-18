9bad9f2f17100c797d6d27c2b0effad8af2940dc
myfluxi <linflux@arcor.de>
2014-08-18 16:18:48 +0200

    msm: thermal: Check temperature only if probed
    This avoids a (harmless) error:
    
    <3>[    0.214144] msm_thermal:msm_thermal_get_freq_table error reading cpufreq table
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -3528,6 +3528,9 @@ static void check_temp(struct work_struct *work)
 	long temp = 0;
 	int ret = 0;
 
+	if (!msm_thermal_probed)
+		return;
+
 	do_therm_reset();
 	if (!msm_thermal_probed)
  		return;
