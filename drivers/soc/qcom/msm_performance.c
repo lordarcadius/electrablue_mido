bf2114349107b0d202c103e26f23bf0e5c6503ef
flar2 <asegaert@gmail.com>
2015-12-05 21:12:48 -0500

    msm_performance: Make input boosting optional
    Signed-off-by: flar2 <asegaert@gmail.com>
    Signed-off-by: ahmedradaideh <ahmed.radaideh@gmail.com>

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -30,7 +30,7 @@ static unsigned int use_input_evts_with_hi_slvt_detect;
 static int touchboost = 1;
 
 static struct mutex managed_cpus_lock;
-
+static int touchboost = 1;
 
 /* Maximum number to clusters that this module will manage*/
 static unsigned int num_clusters;
@@ -190,6 +190,29 @@ static struct input_handler *handler;
 
 
 /**************************sysfs start********************************/
+
+static int set_touchboost(const char *buf, const struct kernel_param *kp)
+{
+	int val;
+
+	if (sscanf(buf, "%d\n", &val) != 1)
+		return -EINVAL;
+
+	touchboost = val;
+
+	return 0;
+}
+
+static int get_touchboost(char *buf, const struct kernel_param *kp)
+{
+	return snprintf(buf, PAGE_SIZE, "%d", touchboost);
+}
+
+static const struct kernel_param_ops param_ops_touchboost = {
+	.set = set_touchboost,
+	.get = get_touchboost,
+};
+device_param_cb(touchboost, &param_ops_touchboost, NULL, 0644);
 
 static int set_num_clusters(const char *buf, const struct kernel_param *kp)
 {
@@ -380,6 +403,10 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	struct cpufreq_policy policy;
 	cpumask_var_t limit_mask;
 	int ret;
+	const char *reset = "0:0 4:0";
+
+	if (touchboost == 0)
+		cp = reset;
 
 	const char *reset = "0:0 2:0";
 
