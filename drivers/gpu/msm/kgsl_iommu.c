f302f1f528e6ebb051cf31fcfe5c37e01ffa576d
Hareesh Gundu <hareeshg@codeaurora.org>
2017-01-02 22:23:08 +0530

    msm: kgsl: Remove BUG_ON from the map global
    Return error instead of BUG_ON on map global failure.
    This will avoid crashing the entire system.
    
    CRs-Fixed: 1106621
    Change-Id: I693dc196bb7a2e01f6a033f5fbbf7b454108108d
    Signed-off-by: Hareesh Gundu <hareeshg@codeaurora.org>
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- drivers/gpu/msm/kgsl_iommu.c
+++ drivers/gpu/msm/kgsl_iommu.c
@@ -135,7 +135,7 @@ static void kgsl_iommu_unmap_globals(struct kgsl_pagetable *pagetable)
 	}
 }
 
-static void kgsl_iommu_map_globals(struct kgsl_pagetable *pagetable)
+static int kgsl_iommu_map_globals(struct kgsl_pagetable *pagetable)
 {
 	unsigned int i;
 
@@ -144,9 +144,11 @@ static void kgsl_iommu_map_globals(struct kgsl_pagetable *pagetable)
 			int ret = kgsl_mmu_map(pagetable,
 					global_pt_entries[i].memdesc);
 
-			BUG_ON(ret);
+			if (ret)
+				return ret;
 		}
 	}
+	return 0;
 }
 
 static void kgsl_iommu_unmap_global_secure_pt_entry(struct kgsl_pagetable
@@ -159,16 +161,16 @@ static void kgsl_iommu_unmap_global_secure_pt_entry(struct kgsl_pagetable
 
 }
 
-static void kgsl_map_global_secure_pt_entry(struct kgsl_pagetable *pagetable)
+static int kgsl_map_global_secure_pt_entry(struct kgsl_pagetable *pagetable)
 {
-	int ret;
+	int ret = 0;
 	struct kgsl_memdesc *entry = kgsl_global_secure_pt_entry;
 
 	if (entry != NULL) {
 		entry->pagetable = pagetable;
 		ret = kgsl_mmu_map(pagetable, entry);
-		BUG_ON(ret);
 	}
+	return ret;
 }
 
 static void kgsl_iommu_remove_global(struct kgsl_mmu *mmu,
@@ -1173,7 +1175,7 @@ static int _init_global_pt(struct kgsl_mmu *mmu, struct kgsl_pagetable *pt)
 		goto done;
 	}
 
-	kgsl_iommu_map_globals(pt);
+	ret = kgsl_iommu_map_globals(pt);
 
 done:
 	if (ret)
@@ -1233,7 +1235,7 @@ static int _init_secure_pt(struct kgsl_mmu *mmu, struct kgsl_pagetable *pt)
 	ctx->regbase = iommu->regbase + KGSL_IOMMU_CB0_OFFSET
 			+ (cb_num << KGSL_IOMMU_CB_SHIFT);
 
-	kgsl_map_global_secure_pt_entry(pt);
+	ret = kgsl_map_global_secure_pt_entry(pt);
 
 done:
 	if (ret)
@@ -1298,7 +1300,7 @@ static int _init_per_process_pt(struct kgsl_mmu *mmu, struct kgsl_pagetable *pt)
 		goto done;
 	}
 
-	kgsl_iommu_map_globals(pt);
+	ret = kgsl_iommu_map_globals(pt);
 
 done:
 	if (ret)
