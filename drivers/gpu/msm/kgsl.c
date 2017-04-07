f13b55226b02ce19c3e51e6eac185d72bb3bbedd
Sunil Khatri <sunilkh@codeaurora.org>
2017-04-07 17:00:55 +0530

    msm: kgsl: Fix kgsl memory allocation and free race condition
    When allocating userspace memory keep reference to memory
    allocation till it is completely initialized and info is sent back
    to userspace.
    
    Change-Id: Id72c82bf98c094ecbd4722813c732a998dcbb188
    Signed-off-by: Tarun Karra <tkarra@codeaurora.org>
    Signed-off-by: Sunil Khatri <sunilkh@codeaurora.org>
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -3051,6 +3051,7 @@ long kgsl_ioctl_gpuobj_alloc(struct kgsl_device_private *dev_priv,
 
 	/* put the extra refcount for kgsl_mem_entry_create() */
 	kgsl_mem_entry_put(entry);
+
 	return 0;
 }
 
@@ -3076,6 +3077,7 @@ long kgsl_ioctl_gpumem_alloc(struct kgsl_device_private *dev_priv,
 
 	/* put the extra refcount for kgsl_mem_entry_create() */
 	kgsl_mem_entry_put(entry);
+
 	return 0;
 }
 
@@ -3101,6 +3103,7 @@ long kgsl_ioctl_gpumem_alloc_id(struct kgsl_device_private *dev_priv,
 
 	/* put the extra refcount for kgsl_mem_entry_create() */
 	kgsl_mem_entry_put(entry);
+
 	return 0;
 }
 
@@ -3221,7 +3224,9 @@ long kgsl_ioctl_cff_syncmem(struct kgsl_device_private *dev_priv,
 
 	kgsl_cffdump_syncmem(dev_priv->device, entry, offset, len, true);
 
+	/* put the extra refcount for kgsl_mem_entry_create() */
 	kgsl_mem_entry_put(entry);
+
 	return 0;
 }
 
