95dda28f7e368dcecc2ac266b8fe816fd9da4a0f
BrateloSlava <d_slava@list.ru>
2017-07-18 16:16:07 +0300

    cpufreq: interactive: fix after add screen off max frequency

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -31,6 +31,7 @@
 #include <linux/workqueue.h>
 #include <linux/kthread.h>
 #include <linux/slab.h>
+#include <linux/display_state.h>
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/cpufreq_interactive.h>
