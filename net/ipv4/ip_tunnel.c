f10c1946f3f4e8c328a3bb6282527d0cc0312782
Sultan Alsawaf <sultanxda@gmail.com>
2018-06-06 15:56:54 -0700

    ip_tunnel: Fix name string concatenate in __ip_tunnel_create()
    By passing a limit of 2 bytes to strncat, strncat is limited to writing
    fewer bytes than what it's supposed to append to the name here.
    
    Since the bounds are checked on the line above this, just remove the string
    bounds checks entirely since they're unneeded.
    
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- net/ipv4/ip_tunnel.c
+++ net/ipv4/ip_tunnel.c
@@ -302,16 +302,17 @@ static struct net_device *__ip_tunnel_create(struct net *net,
 	struct net_device *dev;
 	char name[IFNAMSIZ];
 
-	err = -E2BIG;
 	if (parms->name[0]) {
 		if (!dev_valid_name(parms->name))
 			goto failed;
 		strlcpy(name, parms->name, IFNAMSIZ);
 	} else {
-		if (strlen(ops->kind) > (IFNAMSIZ - 3))
+		if (strlen(ops->kind) > (IFNAMSIZ - 3)) {
+			err = -E2BIG;
 			goto failed;
-		strlcpy(name, ops->kind, IFNAMSIZ);
-		strncat(name, "%d", 2);
+		}
+		strcpy(name, ops->kind);
+		strcat(name, "%d");
 	}
 
 	ASSERT_RTNL();
