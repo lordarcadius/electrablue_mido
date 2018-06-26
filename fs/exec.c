39a521d54f416989ac38bbcb0adfcff6c551164f
Sultan Alsawaf <sultan@kerneltoast.com>
2018-06-27 00:28:42 +0900

    kernel: Boost all CPUs to the max when userspace launches an app
    Zygote is Android's application process, and it only forks when launching
    a new app for the first time. To accelerate app launches, boost all CPUs
    to the max for 1250 ms when zygote decides to fork.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- fs/exec.c
+++ fs/exec.c
@@ -75,7 +75,8 @@ static DEFINE_RWLOCK(binfmt_lock);
 #define ZYGOTE64_BIN	"/system/bin/app_process64"
 static atomic_t zygote32_pid;
 static atomic_t zygote64_pid;
- bool is_zygote_pid(pid_t pid)
+
+bool is_zygote_pid(pid_t pid)
 {
 	return atomic_read(&zygote32_pid) == pid ||
 		atomic_read(&zygote64_pid) == pid;
@@ -1576,11 +1577,6 @@ static int do_execve_common(struct filename *filename,
 	retval = exec_binprm(bprm);
 	if (retval < 0)
 		goto out;
-
-	if (is_su && capable(CAP_SYS_ADMIN)) {
-		current->flags |= PF_SU;
-		su_exec();
-	}
 
 	if (capable(CAP_SYS_ADMIN)) {
 		if (unlikely(!strcmp(filename->name, ZYGOTE32_BIN)))
--- kernel/fork.c
+++ kernel/fork.c
@@ -75,6 +75,8 @@
 #include <linux/uprobes.h>
 #include <linux/aio.h>
 #include <linux/compiler.h>
+#include <linux/cpufreq.h>
+#include <linux/cpu_input_boost.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -1692,6 +1694,10 @@ long do_fork(unsigned long clone_flags,
 	struct task_struct *p;
 	int trace = 0;
 	long nr;
+
+	/* Boost CPU to the max for 1250 ms when userspace launches an app */
+	if (is_zygote_pid(current->pid))
+		cpu_input_boost_kick_max(1250);
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
