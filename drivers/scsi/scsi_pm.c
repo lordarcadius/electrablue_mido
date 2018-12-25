ce8c452ebfba453b101de40a262ce0bbbf237eb5
Vipul Jha <vipuljha08@gmail.com>
2018-12-26 02:15:07 +0530

    Revert "Revert "SCSI: Fix NULL pointer dereference in runtime PM""
    This reverts commit 3cc06f5124d3708ce8a68730f69af46d5f300d50.

--- drivers/scsi/scsi_pm.c
+++ drivers/scsi/scsi_pm.c
@@ -263,8 +263,8 @@ static int sdev_runtime_suspend(struct device *dev)
 		if (err)
 			return err;
 		err = pm->runtime_suspend(dev);
-	blk_post_runtime_suspend(sdev->request_queue, err);
-
+		blk_post_runtime_suspend(sdev->request_queue, err);
+	}
 	return err;
 }
 
@@ -293,8 +293,8 @@ static int sdev_runtime_resume(struct device *dev)
 	if (pm && pm->runtime_resume) {
 		blk_pre_runtime_resume(sdev->request_queue);
 		err = pm->runtime_resume(dev);
-	blk_post_runtime_resume(sdev->request_queue, err);
-
+		blk_post_runtime_resume(sdev->request_queue, err);
+	}
 	return err;
 }
 
