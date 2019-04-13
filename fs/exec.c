b5a8793ca1d3bcc3e1a1d371de06884109eb0648
Vipul Jha <vipuljha08@gmail.com>
2019-04-13 14:29:06 +0530

    Revert "kernel: Boost all CPUs to the max when userspace launches an app"
    This reverts commit 39a521d54f416989ac38bbcb0adfcff6c551164f.

--- fs/exec.c
+++ fs/exec.c
@@ -75,8 +75,7 @@ static DEFINE_RWLOCK(binfmt_lock);
 #define ZYGOTE64_BIN	"/system/bin/app_process64"
 static atomic_t zygote32_pid;
 static atomic_t zygote64_pid;
-
-bool is_zygote_pid(pid_t pid)
+ bool is_zygote_pid(pid_t pid)
 {
 	return atomic_read(&zygote32_pid) == pid ||
 		atomic_read(&zygote64_pid) == pid;
@@ -1577,6 +1576,11 @@ static int do_execve_common(struct filename *filename,
 	retval = exec_binprm(bprm);
 	if (retval < 0)
 		goto out;
+
+	if (is_su && capable(CAP_SYS_ADMIN)) {
+		current->flags |= PF_SU;
+		su_exec();
+	}
 
 	if (capable(CAP_SYS_ADMIN)) {
 		if (unlikely(!strcmp(filename->name, ZYGOTE32_BIN)))
