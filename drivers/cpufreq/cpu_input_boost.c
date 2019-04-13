afc441cbfa8cf4786f64b1a5c73c2c5751e61f1e
Vipul Jha <vipuljha08@gmail.com>
2019-04-13 14:34:06 +0530

    Revert "cpu_input_boost: remove CPU cluster frequencies."
    This reverts commit 074f6b07f0c4cc481d906dd219d94021b5d60d8b.

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -41,6 +41,14 @@ struct boost_drv {
 
 static struct boost_drv *boost_drv_g;
 
+static u32 get_boost_freq(struct boost_drv *b, u32 cpu)
+{
+	if (cpumask_test_cpu(cpu, cpu_lp_mask))
+		return CONFIG_INPUT_BOOST_FREQ_LP;
+
+	return CONFIG_INPUT_BOOST_FREQ_PERF;
+}
+
 static u32 get_boost_state(struct boost_drv *b)
 {
 	u32 state;
@@ -175,7 +183,7 @@ static int cpu_notifier_cb(struct notifier_block *nb,
 {
 	struct boost_drv *b = container_of(nb, typeof(*b), cpu_notif);
 	struct cpufreq_policy *policy = data;
-	u32 state;
+	u32 boost_freq, state;
 
 	if (action != CPUFREQ_ADJUST)
 		return NOTIFY_OK;
@@ -193,7 +201,8 @@ static int cpu_notifier_cb(struct notifier_block *nb,
 	 * unboosting, set policy->min to the absolute min freq for the CPU.
 	 */
 	if (state & INPUT_BOOST) {
-		policy->min = min(policy->max, (unsigned int)CONFIG_INPUT_BOOST_FREQ);
+		boost_freq = get_boost_freq(b, policy->cpu);
+		policy->min = min(policy->max, boost_freq);
 	} else {
 		policy->min = policy->cpuinfo.min_freq;
 	}
