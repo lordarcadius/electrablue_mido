a70f5119dd671e0161a79bfc972d96a04ea209f9
Vipul Jha <vipuljha08@gmail.com>
2019-07-05 16:48:22 +0530

    Revert "misc: goodix: Offload fb notifier onto worker"
    This reverts commit 28e5359071a89d7d1c0a227b81e490c22e1f3b30.

--- drivers/input/fingerprint/goodix_mido/gf_spi.h
+++ drivers/input/fingerprint/goodix_mido/gf_spi.h
@@ -86,7 +86,6 @@ struct gf_dev {
 	struct fasync_struct *async;
 #endif
 	struct notifier_block notifier;
-	struct work_struct fb_work;
 	char device_available;
 	char fb_black;
 	struct wake_lock ttw_wl;
