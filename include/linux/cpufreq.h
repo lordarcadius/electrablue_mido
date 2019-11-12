5c42aedcaaf9772d00ec3741b676aa3335d53d0d
GreatLifehacker <gowrikingofworld@gmail.com>
2019-11-12 09:05:12 +0530

    Added Conservativex Governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -519,6 +519,9 @@ extern struct cpufreq_governor cpufreq_gov_sched;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELECTRON)
 extern struct cpufreq_governor cpufreq_gov_electron;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_electron)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
+extern struct cpufreq_governor cpufreq_gov_conservativex;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_conservativex)
 #endif
 
 /*********************************************************************
